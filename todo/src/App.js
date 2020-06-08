5510c65d5dfd05c73da5ed263039b727e0edcb04
lateralcreativity <lateralcreativity@outlook.com>
2020-06-08 17:52:33 -0500

    mvp

---
+++ todo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainContent from './components/MainContent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
